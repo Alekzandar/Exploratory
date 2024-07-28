2394339c3b6546b5c9c81d6a18fd16a5b871f30d
Alekzandar <alekzandar@live.com>
2024-07-27 20:26:46 -0600

    Firing along

---
+++ Bash_Scripting/Intro/variablescript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+
+now=$(date)
+
+echo "The system time and date is $now."
+
+myname="Aleks"
+mytype="fruit"
+
+echo "Hello, my name is $myname, and my type is $mytype."
+
+echo "Username is: $USER"
