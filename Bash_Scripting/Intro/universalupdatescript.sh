f918e6648b02e64cecb55d4942f1b9005d5a819d
alekzandar <alekzandar@live.com>
2024-07-28 00:28:51 -0600

    Edits

--- Bash_Scripting/Intro/universalupdatescript.sh
+++ Bash_Scripting/Intro/universalupdatescript.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-if [ -d /etc/pacman.d ]
+release_file=/etc/os-release
+
+
+#-q quiet mode, don't print, grep global editor for regexp and print
+if grep -q "Arch" $release_file
 then
 	#Host based on Arch, run pacman update command
 	sudo pacman -Syu
 fi
 
-if [ -d /etc/apt ]
+#To make the script work on both Debian or Ubuntu (redundant)
+if grep -q "Debian" $release_file
+then
+	sudo apt update
+	sudo apt dis-upgrade
+fi
+
+if grep -q "Ubuntu" $release_file
 then
 	#Host based on Debian or Ubuntu,
 	#Run apt version of command
