4f943cfcf3b20fb66d3f56152d8f0119ccab5042
alekzandar <alekzandar@live.com>
2024-07-30 21:06:27 -0600

    Error handling

---
+++ Bash_Scripting/Intro/streamscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 1 is standard output, 2 is standard error, 1 is everything that is not an error, 2 is everything that is an error 
+# 2> /dev/null, a black hole, nothing that goes in comes out
+# find /etc -type f 2> /dev/null, standard errors are sent into the black hole
+
+#find /etc -type f 1>find_output.txt 2>find_errors.txt
+
+sudo 
--- Bash_Scripting/Intro/universalupdatescript.sh
+++ Bash_Scripting/Intro/universalupdatescript.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 release_file=/etc/os-release
-
+logfile = /var/log/updater.log
+errorlog = /var/log/updater_errors.log
 
 #-q quiet mode, don't print, grep global editor for regexp and print
 if grep -q "Arch" $release_file
 then
 	#Host based on Arch, run pacman update command
-	sudo pacman -Syu
+	sudo pacman -Syu 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+		echo "An error occured, please check $errorlog file."
+	fi
 fi
 
 #To make the script work on both Debian or Ubuntu (redundant)
 if grep -q "Debian" $release_file || grep -q "Ubuntu" $release_file
 then
-	sudo apt update
-	sudo apt dis-upgrade
+	sudo apt update 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+		echo "An error occured, please check the $errorlog file."
+	fi
+	sudo apt dis-upgrade 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+		echo "An error occured, please the $errorlog file."
+	fi
 fi
 
